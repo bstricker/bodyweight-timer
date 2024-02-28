95a9a5f6607b01d7a86a878fe5da52d7f9dc7255
Benedikt Stricker <benedikt.stricker@gmx.at>
2024-02-28 20:33:02 +0100

    build: update targetSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.stricker.bodyweighttimer"
         minSdk = 31
-        targetSdk = 31
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
